629abd2140d6b2751dad16a27ca91c7627be36fd
OlyaJ <schastnayaolga92@gmail.com>
2025-02-27 18:11:10 +0300

    fixes in textArea

--- components/TextArea/TextArea.tsx
+++ components/TextArea/TextArea.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import s from './TextArea.module.scss'
+'use client'
+
+import React, { useState } from 'react';
+import s from './TextArea.module.scss'; 
+import clsx from 'clsx';
 
 
 type Props = {
   label: string
-  placeholder: string
+  width: string
+  placeholder?: string
   showError?: boolean
   error?: string
   disabled?: boolean
+  showCharacterCount?: boolean
 }
 
-export const TextArea = ({showError, label, placeholder, error, disabled}: Props) => {
+export const TextArea = ({showError, label, placeholder, error, disabled, width, showCharacterCount}: Props) => {
+  const [characterCount, setCharacterCount] = useState(0)
+
+  const handleChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCharacterCount(e.currentTarget.value.length)
+  }
+
   return (
-    <div className = {s.textAreaContainer}>
+    <div className = {s.textAreaContainer}  style={{ width }}>
       <label className = {s.label}>{label}</label>
-        <textarea className={`${s.textArea} ${showError ? s.error: ''}`} placeholder={placeholder} disabled={disabled}/>
+        <textarea className={clsx(s.textArea, { [s.error]: showError })} placeholder={placeholder} 
+        disabled={disabled} onChange={handleChange}  style={{ width }}/>
+        { showCharacterCount && (<div className={s.character}>
+          {characterCount}/500
+        </div>
+)}
         {showError && <span className={s.error}>{error}</span>}
     </div>
   );
