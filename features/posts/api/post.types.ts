ec6d3d69a7667636dfbc984bd0a0001b419bbc6f
OksanaSokal <sokaloksana@gmail.com>
2025-03-19 11:23:24 +0000

    fix: fix mistakes

--- features/posts/api/post.types.ts
+++ features/posts/api/post.types.ts
@@ -34,3 +34,53 @@ export type Owner = {
       uploadId: string
     }[], 
   }
+
+
+
+export type Images = {
+  url: string
+  width: number
+  height: number
+  fileSize: number
+  createdAt: string
+  uploadId: string
+}
+
+export type Responce = {
+  id: number
+  userName: string
+  description: string
+  location: string
+  images: Images[]
+  createdAt: string
+  updatedAt: string
+  ownerId: number
+  avatarOwner: string
+  owner: Owner
+  likesCount: number
+  isLiked: boolean
+  avatarWhoLikes: boolean
+}
+
+export type Items = {
+  id: number;
+  userName: string;
+  description: string;
+  location: string;
+  images: Images[];
+  createdAt: string;
+  updatedAt: string;
+  ownerId: number;
+  avatarOwner: string;
+  owner: Owner;
+  likesCount: number;
+  isLiked: boolean;
+  avatarWhoLikes: boolean;
+}
+
+export type ResponceAllPosts =  {
+  totalCount: number;
+  pageSize: number;
+  totalUsers: number;
+  items: Items[];
+}
