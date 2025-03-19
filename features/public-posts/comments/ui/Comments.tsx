e4a0e100297e37f89292cd32612a9a285ea0329f
Funprod <dron12033@gmail.com>
2025-03-19 13:21:04 +0300

    fix

--- features/public-posts/comments/ui/Comments.tsx
+++ features/public-posts/comments/ui/Comments.tsx
@@ -6,15 +6,37 @@ import noImg from '@/assets/icons/noImg.png'
 import s from './Comments.module.scss'
 import { CommentsType } from '../types'
 import { getTimeAgo } from '@/shared/lib/getTimeAgo'
+import { PostType } from '../../post/types'
 
 type CommentsProps = {
   comments: CommentsType
+  post: PostType
 }
-function Comments({ comments }: CommentsProps) {
+function Comments({ comments, post }: CommentsProps) {
   return (
     <div>
       <hr className={s.hr} />
       <ul className={s.comments}>
+        {post.description && (
+          <div className={s.commentsList}>
+            <div>
+              <Image
+                src={post.avatarOwner ? post.avatarOwner : noImg}
+                alt=""
+                width={36}
+                height={36}
+                style={{ borderRadius: '50%', marginRight: '12px' }}
+              />
+            </div>
+            <div>
+              <span className={s.userName}>{post.userName} </span>
+              <span>{post.description}</span>
+              <div>
+                <time className={s.time}>{getTimeAgo(post.createdAt)}</time>
+              </div>
+            </div>
+          </div>
+        )}
         {comments.items.map((item) => (
           <li key={item.id} className={s.commentsList}>
             <div>
