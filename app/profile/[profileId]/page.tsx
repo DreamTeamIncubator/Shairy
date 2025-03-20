32dd1b59f6a04a31b7a3db850c1a26c8ee7b167b
iplatos <platonoff9168@gmail.com>
2025-03-20 12:22:30 +0300

    feat: public-user page implemented in PublicProfilePage

--- app/profile/[profileId]/page.tsx
+++ app/profile/[profileId]/page.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import ImageWithoutAvatar from '../../../assets/icons/noImg.png'
 import { getAdditionalData, getProfilePublicUser } from '@/features/public-user/api/public-userApi'
+import { PublicUser } from '@/features/public-user/ui/public-user'
+
 type PageProps = {
   params: {
     profileId: string
@@ -16,74 +16,5 @@ export default async function PublicProfilePage({ params }: PageProps) {
   if (!profileData) {
     return <div>Profile not found</div>
   }
-  return (
-    <div style={{ backgroundColor: 'green', display: 'flex', justifyContent: 'center' }}>
-      <div style={{ backgroundColor: 'red' }}>
-        <div
-          style={{
-            backgroundColor: 'pink',
-            display: 'flex',
-            justifyContent: 'row',
-            padding: '30px',
-          }}>
-          {profileData.avatars.length ? (
-            <Image src={profileData.avatars[0]} alt={'avatar'} width={204} height={204} />
-          ) : (
-            <Image
-              style={{ borderRadius: 30 }}
-              alt="posts"
-              src={ImageWithoutAvatar}
-              width={204}
-              height={204}
-            />
-          )}
-          <div
-            style={{
-              display: 'flex',
-              backgroundColor: 'yellow',
-              flexDirection: 'column',
-              paddingInlineStart: '30px',
-            }}>
-            <h1> {profileData.userName}</h1>
-            <div
-              style={{
-                backgroundColor: 'green',
-                display: 'flex',
-                marginTop: '20px',
-                marginBottom: '30px',
-                gap: '100px',
-              }}>
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <p>{profileData.userMetadata.followers}</p>
-                <p>Followers </p>
-              </div>
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <p>{profileData.userMetadata.following}</p>
-                <p>Following </p>
-              </div>
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <p>{profileData.userMetadata.publications}</p>
-                <p>Publications </p>
-              </div>
-            </div>
-            {/* <p>{profileData.description}</p> */}
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptate tenetur
-              facilis eius. Delectus sint pariatur ad atque molestias fugit, accusamus mollitia
-              explicabo. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
-        </div>
-        {/* Отображение публикаций и других данных */}
-        {additionalData.items.map((post: any) => (
-          <div key={post.id}>
-            {post.description}
-            {post.images.map((image: any, index: number) => (
-              <Image alt="posts" key={index} src={image.url} width={234} height={228} />
-            ))}
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+  return <PublicUser profileData={profileData} additionalData={additionalData} />
 }
--- features/public-user/ui/publick-user.tsx
+++
@@ -1,3 +0,0 @@
-export const PublicUser = () => {
-  return <div>public</div>
-}
