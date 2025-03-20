74afef70b9af7f974498736b05c9c29bd7d0f304
Funprod <dron12033@gmail.com>
2025-03-20 15:30:36 +0300

    git checkout SAM-23

--- app/my-profile/[id]/page.tsx
+++ app/my-profile/[id]/page.tsx
@@ -1,11 +1,11 @@
 'use client'
-import React, {useEffect, useRef, useState} from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import s from './myProfile.module.scss'
-import {ModalRadix} from '@/shared/ui/Modal/ModalRadix'
-import {useParams} from 'next/navigation'
-import {useGetAllUsersPostsQuery} from '@/features/posts/api/post';
+import { ModalRadix } from '@/shared/ui/Modal/ModalRadix'
+import { useParams } from 'next/navigation'
+import { useGetAllUsersPostsQuery } from '@/features/posts/api/post'
 import ProfileDescription from '@/features/profile/ui/ProfileDescription'
-import Post from '@/features/posts/ui/Post';
+import Post from '@/features/posts/ui/Post'
 
 const MyProfile = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -13,14 +13,14 @@ const MyProfile = () => {
   const [endCursorPostId, setEndCursorPostId] = useState<any>(null)
   const { id } = useParams()
 
-  const {data: allPosts} = useGetAllUsersPostsQuery({
-      pageSize: 8,
-      endCursorPostId,
-      userId: +id
+  const { data: allPosts } = useGetAllUsersPostsQuery({
+    pageSize: 8,
+    endCursorPostId,
+    userId: +id,
   })
-console.log(allPosts)
+  console.log(allPosts)
   const lastPostRef = useRef<HTMLDivElement | null>(null)
-    const [postId, setPostId] =useState<null | number>(null)
+  const [postId, setPostId] = useState<null | number>(null)
 
   useEffect(() => {
     if (!lastPostRef.current) return
@@ -49,14 +49,14 @@ console.log(allPosts)
   const onClickHandler = (id) => {
     //TODO: open post if user is authorised
     setIsOpen(true)
-      setPostId(id)
+    setPostId(id)
   }
   if (!id) {
     return <div>Loading...</div>
   }
   return (
     <>
-       <ProfileDescription />
+      <ProfileDescription />
       <div className={s.postsWrapper}>
         {allPosts?.items.map((item, index) => {
           const isLastPost = index === allPosts.items.length - 1
@@ -65,15 +65,15 @@ console.log(allPosts)
               key={item.id}
               ref={isLastPost ? lastPostRef : null}
               className={s.post}
-              onClick={()=>onClickHandler(item.id)}>
+              onClick={() => onClickHandler(item.id)}>
               <img src={item?.images[0]?.url} alt="photo-post" className={s.img} />
             </div>
           )
         })}
       </div>
-        <ModalRadix open={isOpen} onClose={() => setIsOpen(false)} modalTitle={''} >
-            <Post postId={postId} open={true} onClose={()=>console.log('onclose')}/>
-        </ModalRadix>
+      <ModalRadix open={isOpen} onClose={() => setIsOpen(false)} modalTitle={''}>
+        <Post postId={postId} open={true} onClose={() => console.log('onclose')} />
+      </ModalRadix>
     </>
   )
 }
