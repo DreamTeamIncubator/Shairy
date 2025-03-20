97424b839827e84785a8eed6d27431150682414a
iplatos <platonoff9168@gmail.com>
2025-03-20 12:19:02 +0300

    feat: types for public page has been created

---
+++ features/public-user/types.ts
@@ -0,0 +1,60 @@
+type Avatar = {
+  url: string
+  width: number
+  height: number
+  fileSize: number
+  createdAt: string // ISO format date
+}
+
+type UserMetadata = {
+  following: number
+  followers: number
+  publications: number
+}
+
+type UserProfileData = {
+  id: number
+  userName: string
+  aboutMe: string
+  avatars: Avatar[]
+  userMetadata: UserMetadata
+  hasPaymentSubscription: boolean
+}
+
+export type PropsType = {
+  profileData: UserProfileData
+  additionalData: AdditionalData
+}
+type Image = {
+  url: string
+  width: number
+  height: number
+  fileSize: number
+  createdAt: string
+  uploadId: string
+}
+
+type Owner = {
+  firstName: string
+  lastName: string
+}
+
+type Item = {
+  id: number
+  userName: string
+  description: string
+  location: string
+  images: Image[]
+  createdAt: string // ISO format date
+  updatedAt: string // ISO format date
+  ownerId: number
+  avatarOwner: string
+  owner: Owner
+}
+
+type AdditionalData = {
+  totalCount: number
+  pageSize: number
+  totalUsers: number
+  items: Item[]
+}
