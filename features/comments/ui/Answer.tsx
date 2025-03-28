ec36e9caf776416c68c7de8fdbe8227de5db0af2
iplatos <platonoff9168@gmail.com>
2025-03-28 11:58:26 +0300

    fix: img was replacer with Image

--- features/comments/ui/Answer.tsx
+++ features/comments/ui/Answer.tsx
@@ -1,30 +1,32 @@
-import Image from "next/image";
-import { formatDistanceToNow } from "date-fns";
-import s from "./AnswerItem.module.scss";
-import { CommentAnswerItem } from "@/features/comments/api/comments.types";
-import { useLikeStatus } from "../lib/hooks/useLikeStatus";
+import Image from 'next/image'
+import { formatDistanceToNow } from 'date-fns'
+import s from './AnswerItem.module.scss'
+import { CommentAnswerItem } from '@/features/comments/api/comments.types'
+import { useLikeStatus } from '../lib/hooks/useLikeStatus'
 
 type AnswerProps = {
   answer: CommentAnswerItem
   onLike: (answerId: number, isLiked: boolean) => void
-  
-};
+}
 
-const AnswerItem = ({ answer, onLike} : AnswerProps) => {
-
+const AnswerItem = ({ answer, onLike }: AnswerProps) => {
   const { isLiked, likeCount, toggleLike } = useLikeStatus({
     initialIsLiked: answer.isLiked,
     initialLikeCount: answer.likeCount,
     onLikeToggle: (id, newIsLiked) => onLike(id, newIsLiked),
-  });
-
-  
+  })
 
   return (
     <div className={s.answer}>
       <div className={s.avatarCommentWrapper}>
         {answer.from.avatars?.[0]?.url ? (
-          <img src={answer.from.avatars[0].url} alt="Avatar" className={s.avatar} />
+          <Image
+            src={answer.from.avatars[0].url}
+            alt="Avatar"
+            width={36}
+            height={36}
+            className={s.avatar}
+          />
         ) : (
           <div className={s.defaultAvatar}></div>
         )}
@@ -33,24 +35,26 @@ const AnswerItem = ({ answer, onLike} : AnswerProps) => {
             <span className={s.answerUserName}>{answer.from.username}</span> {answer.content}
           </p>
           <Image
-            src={isLiked ? "/redHeart.svg" : "/heart.svg"}
+            src={isLiked ? '/redHeart.svg' : '/heart.svg'}
             alt="heart"
             width={16}
             height={16}
             className={s.heart}
-            onClick={()=>toggleLike(answer.id)}
+            onClick={() => toggleLike(answer.id)}
           />
         </div>
       </div>
       <div className={s.dateLikeContainer}>
-          <span className={s.date}>
-            {answer.createdAt ? formatDistanceToNow(new Date(answer.createdAt), { addSuffix: true }) : ""}
-          </span>
-          {likeCount > 0 && <span className={s.like}>Likes: {likeCount}</span>}
-          <span className={s.like}  >Answer</span>
-        </div>
+        <span className={s.date}>
+          {answer.createdAt
+            ? formatDistanceToNow(new Date(answer.createdAt), { addSuffix: true })
+            : ''}
+        </span>
+        {likeCount > 0 && <span className={s.like}>Likes: {likeCount}</span>}
+        <span className={s.like}>Answer</span>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default AnswerItem;
+export default AnswerItem
