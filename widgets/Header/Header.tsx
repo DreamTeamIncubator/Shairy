96dae05f9721d9ef84bda769f4c8825d6647130f
iplatos <platonoff9168@gmail.com>
2025-04-03 12:49:24 +0300

    feat: notificationMenu implemented

--- widgets/Header/Header.tsx
+++ widgets/Header/Header.tsx
@@ -8,18 +8,54 @@ import Link from 'next/link'
 
 import { LanguageSelect } from '@/shared/ui/Select/LanguageSelect/LanguageSelect'
 import { useGetMeQuery } from '@/features/auth/api/auth'
-
+import { NotificationMenu } from '../NotificationMenu/NotificationMenu'
+export type Notification = {
+  id: string
+  isNew?: boolean
+  message: string
+  notificationTime: string
+}
 export const Header = () => {
   const { data } = useGetMeQuery()
   const SignUpForm = () => {
     redirect('/auth/sign-up')
   }
-
+  // const mockedNotifications: Notification[] = [
+  //   {
+  //     id: '0',
+  //     isNew: true,
+  //     message: 'Следующий платеж у вас спишется через 1 день',
+  //     notificationTime: '1 час',
+  //   },
+  //   {
+  //     id: '1',
+  //     isNew: true,
+  //     message: 'Ваша подписка истекает через 7 дней',
+  //     notificationTime: '1 день',
+  //   },
+  //   {
+  //     id: '2',
+  //     isNew: true,
+  //     message: 'Ваша подписка истекает через 7 дней',
+  //     notificationTime: '1 день',
+  //   },
+  //   {
+  //     id: '3',
+  //     message: 'Ваша подписка истекает через 7 дней',
+  //     notificationTime: '1 день',
+  //   },
+  //   {
+  //     id: '4',
+  //     message: 'Ваша подписка истекает через 7 дней',
+  //     notificationTime: '1 день',
+  //   },
+  // ]
   return (
     <div className={s.header}>
       <div className={s.content}>
         <h2 className={s.text}>Shairy</h2>
         <div className={s.navigate}>
+          {data ? <NotificationMenu /> : null}
           <LanguageSelect />
           {data ? null : (
             <div>
